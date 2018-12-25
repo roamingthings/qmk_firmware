c4f446d04749afacb07ef15772bab78c69a49746
Alexander Sparkowsky <info@roamingthings.de>
2018-12-25 12:15:11 +0100

    Modify default files for ripple effect

--- keyboards/massdrop/ctrl/rules.mk
+++ keyboards/massdrop/ctrl/rules.mk
@@ -12,7 +12,8 @@ CUSTOM_MATRIX = yes
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = no       # Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE = no        # Mouse keys(+4700)
+# Mousekeys see https://github.com/valen214/Massdrop-Ctrl-Keyboard-Ripple-LED
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 CONSOLE_ENABLE = no         # Console for debug(+400)
 COMMAND_ENABLE = no         # Commands for debug and configuration
